cebf0bab78408c460b66a2ce333beeca7ecd33ad
Oren Novotny <oren@novotny.org>
2018-11-16 20:10:26 -0500

    Add test projects

---
+++ WinFormsTestApp/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using SingleInstanceHelper;
+
+namespace WinFormsTestApp
+{
+    static class Program
+    {
+        /// <summary>
+        /// The main entry point for the application.
+        /// </summary>
+        [STAThread]
+        static void Main(string[] args)
+        {
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+
+            var first = ApplicationActivator.LaunchOrReturn(otherInstance => { MessageBox.Show("got data"); }, args);
+            if(!first)
+                return; 
+            Application.Run(new Form1());
+        }
+    }
+}
---
+++ WpfTestApp/App.xaml.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows;
+using SingleInstanceHelper;
+
+namespace WpfTestApp
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+        protected override void OnStartup(StartupEventArgs e)
+        {
+
+            var first = ApplicationActivator.LaunchOrReturn(otherInstance => { MessageBox.Show("got data"); }, e.Args);
+            if (!first)
+            {
+                Shutdown();
+            }
+
+
+            base.OnStartup(e);
+        }
+    }
+}
